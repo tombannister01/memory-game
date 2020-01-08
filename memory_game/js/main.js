df5c4036e41be6174b34b924de72dff91440fdc9
tom bannister <tombannister2463@gmail.com>
2020-01-08 21:35:11 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log('up and running')
-let cardOne = 'queen';
-let cardTwo = 'king';
-let cardThree = 'queen';
-let cardFour = 'king';
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+let card0ne = cards[0];
+cardsInPlay.push(card0ne);
+console.log("User flipped " + cardsInPlay[0]);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+if (cardsInPlay.length === 2) {
+	console.log('user has picked two cards');
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again.");
+	}
+} 
