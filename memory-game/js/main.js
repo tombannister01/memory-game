d547da318d1d6889418026e627caf2283100e4d6
tombannister01 <tombannister2463@gmail.com>
2020-01-13 17:11:37 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,24 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+const cards = [{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,16 +30,20 @@ if (cardsInPlay[0]===cardsInPlay[1]) {
 }
 
 function flipcard (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("user flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("user flipped " + cards[cardId].rank)
 	if (cardsInPlay.length === 2) {
 		console.log('user has picked two cards');
 		checkForMatch()
 	}
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	
-	} 
+} 
 
 flipcard(0);
 flipcard(2);
+
+
 
 
